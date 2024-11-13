cf5e196b170d36431b7d79fe2e2bb08faadff70b
nasreena <muhammedoldbeach5@gmail.com>
2024-11-13 12:56:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let element = document.getElementById("main title")
+
+
+console.log(element) 
+
+let element1 = document.getElementsByClassName("container")
+console.log( element1);
+
+let element2 = document.getElementsByClassName("prgrph")
+console.log( element2);
+
+
+let element3 = document.getElementsByTagName("p")
+console.log(element3);
+
+
+let element4 = document.querySelector(".btn")
+console.log(element4);
+
+
+let element5 = document.querySelectorAll(".btn")
+console.log(element5);
+
+let element6 = document.querySelectorAll("p")
+console.log(element6);
+
+
